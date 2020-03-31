5ee172066e7e17921e5cd9893c8b985a0eb7cc41
brenda <bcq.cpp@gmail.com>
2020-03-30 23:20:21 -0300

    variables in p5.js

--- animation.js
+++ animation.js
@@ -1,11 +1,18 @@
 function setup(){
     createCanvas(600, 400);
-
+    // Having the background() function in the setup block, it'll be drawn once.
+    // Different than putting this function in the draw block, which would paint the background over and over again.
     background(255, 60, 125);
 }
 
 function draw(){
-
+    // The position of the ellipse will be the same coordinates of the mouse, both on the x-axis and the y-axis.
     fill(100, 200, 100);
     ellipse(mouseX, mouseY, 30, 30);
+}
+
+// This function works as an event.
+// Once the user presses the mouse, the background will be drawn again, so the screen will be "clear" again.
+function mousePressed(){
+    background(255, 60, 125);
 }
