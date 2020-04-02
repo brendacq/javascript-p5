7f66314fd08e2f278a153f8e0ba9ac11be2d5c9f
brenda <bcq.cpp@gmail.com>
2020-04-01 22:47:51 -0300

    JavaScript objects

---
+++ animation-pt1.js
@@ -0,0 +1,40 @@
+// Creating an object. An object is very useful for us to assemble the attributes of an element at one place.
+var circle = {
+    circleWidth,
+    circleHeight
+}
+
+var bgColor = {
+    r: 255,
+    g: 60,
+    b: 125
+}
+
+var circleColor = {
+    r:100,
+    g: 200,
+    b: 100
+}
+
+function setup(){
+    createCanvas(600, 400);
+    // Having the background() function in the setup block, it'll be drawn once.
+    // Different than putting this function in the draw block, which would paint the background over and over again.
+    background(bgColor.r, bgColor.g, bgColor.b);
+}
+
+function draw(){
+    // Initializing my variables with the value of the mouseX or mouseY coordinates minus some number.
+    // At each time the function draw is executed, the circle width and height will vary according to the mouse coordinates.
+    circle.circleWidth = mouseX - 30;
+    circle.circleHeight = mouseY - 50;
+    // The position of the ellipse will be the same coordinates of the mouse, both on the x-axis and the y-axis.
+    fill(circleColor.r, circleColor.g, circleColor.b);
+    ellipse(mouseX, mouseY, circleWidth, circleHeight);
+}
+
+// This function works as an event.
+// Once the user presses the mouse, the background will be drawn again, so the screen will be "clear" again.
+function mousePressed(){
+    background(255, 60, 125);
+}
