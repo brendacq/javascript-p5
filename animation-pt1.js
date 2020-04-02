931f11a8f0d68e63e9dac4e08436baeb24ac8a66
brenda <bcq.cpp@gmail.com>
2020-04-01 23:48:04 -0300

    Fixing object names and attributes

--- animation-pt1.js
+++ animation-pt1.js
@@ -1,19 +1,16 @@
 // Creating an object. An object is very useful for us to assemble the attributes of an element at one place.
-var circle = {
+var crcl = {
     Width: 0,
-    Height: 0
+    Height: 0,
+    r: 0,
+    g: 200,
+    b: 0
 }
 
 var bgColor = {
     r: 255,
     g: 60,
     b: 125
-}
-
-var circleColor = {
-    r: 0,
-    g: 200,
-    b: 0
 }
 
 function setup(){
@@ -26,15 +23,16 @@ function setup(){
 function draw(){
     // Initializing my variables with the value of the mouseX or mouseY coordinates minus some number.
     // At each time the function draw is executed, the circle width and height will vary according to the mouse coordinates.
-    circle.Width = mouseX - 30;
-    circle.Height = mouseY - 50;
+    crcl.Width = mouseX - 30;
+    crcl.Height = mouseY - 50;
 
-    circleColor.r = map(mouseX, 0, 600, 0, 255);
-    circleColor.b = map(mouseY, 0, 400, 255, 0);
+    // The map function will 
+    crcl.r = map(mouseX, 0, 600, 0, 255);
+    crcl.b = map(mouseY, 0, 400, 255, 0);
 
     // The position of the ellipse will be the same coordinates of the mouse, both on the x-axis and the y-axis.
-    fill(circleColor.r, circleColor.g, circleColor.b);
-    ellipse(mouseX, mouseY, circle.Width, circle.Height);
+    fill(crcl.r, crcl.g, crcl.b);
+    ellipse(mouseX, mouseY, crcl.Width, crcl.Height);
 }
 
 // This function works as an event.
