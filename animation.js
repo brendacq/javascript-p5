92dcfb53560d2a6ad86b94a970de97f662e922ea
brenda <bcq.cpp@gmail.com>
2020-04-01 22:42:52 -0300

    objects introduction

--- animation.js
+++ animation.js
@@ -1,19 +1,26 @@
-// Declaring my own variables
-var circleWidth;
-var circleHeight;
+// Creating an object. An object is very useful for us to assemble the attributes of an element at one place.
+var circle = {
+    circleWidth,
+    circleHeight
+}
 
+var color = {
+    r: 255,
+    g: 60,
+    b: 125
+}
 function setup(){
     createCanvas(600, 400);
     // Having the background() function in the setup block, it'll be drawn once.
     // Different than putting this function in the draw block, which would paint the background over and over again.
-    background(255, 60, 125);
+    background(color.r, color.g, color.b);
 }
 
 function draw(){
     // Initializing my variables with the value of the mouseX or mouseY coordinates minus some number.
     // At each time the function draw is executed, the circle width and height will vary according to the mouse coordinates.
-    circleWidth = mouseX - 30;
-    circleHeight = mouseY - 50;
+    circle.circleWidth = mouseX - 30;
+    circle.circleHeight = mouseY - 50;
     // The position of the ellipse will be the same coordinates of the mouse, both on the x-axis and the y-axis.
     fill(100, 200, 100);
     ellipse(mouseX, mouseY, circleWidth, circleHeight);
