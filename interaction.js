30250ec02be489c5b6ba105b710dc7220d64a70d
brenda <bcq.cpp@gmail.com>
2020-04-04 16:56:17 -0300

    bouncing ball with Else If

--- interaction.js
+++ interaction.js
@@ -1,5 +1,6 @@
 let x = 0, y;
 let speed = 3;
+let r = 250;
 
 function setup(){
     createCanvas(600, 400);
@@ -9,15 +10,19 @@ function setup(){
 function draw(){
     background(255, 180, 200);
 
-    fill(250, 240, 75);
+    fill(r, 240, 75);
     ellipse(x, y, 20, 20);
 
     // The value for the x coordinate of the ball will increase according to the speed value
     x += speed;
     
     // Once the ball reaches one of the edges of the canvas, the speed value will change so the ball will move in the opposite 
-    // direction.
-    if(x==0 || x==width){
+    // direction and also change its color.
+    if(x==0){
         speed *= -1;
+        r = 250;
+    } else if (x==width){
+        speed *= -1;
+        r = 110;
     }
 }
