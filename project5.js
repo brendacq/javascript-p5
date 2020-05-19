aa6cad3bfb536cdde32d9b5f6566adc94bdd886b
brenda <bcq.cpp@gmail.com>
2020-05-18 21:41:15 -0300

    simple project with nested loops

--- project5.js
+++ project5.js
@@ -1,20 +1,20 @@
 // This project was made based on the Nested Loops video from the 4th section of the p5.js course: Interaction.
 // The idea is to make a matrix with different elements depending on their position.
 
-function setup(){
-    createCanvas (400, 400);
+function setup() {
+    createCanvas(400, 400);
     background(200, 255, 190);
 }
 
-function draw(){    
+function draw() {
     strokeWeight(0.1);
-    for(var i=0; i<=width; i+=30){
-        for(var j=0; j<=height; j+=30){
-            if(i==j){
+    for (var i = 0; i <= width; i += 30) {
+        for (var j = 0; j <= height; j += 30) {
+            if (i == j) {
                 fill(200, 210, 110);
                 rectMode(CENTER);
                 rect(i, j, 13, 13);
-            } else if (i>j){
+            } else if (i > j) {
                 fill(240, 190, 172);
                 ellipse(i, j, 13, 13);
             } else {
